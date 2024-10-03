989a13b89b12a01fd6c04fda3a7089ba01b8ea21
Vinicius Santos <vdosantos12@hotmail.com>
2024-10-02 21:45:15 -0300

    adds BindingHandler class responsible for data binding the view to ViewModel

---
+++ API/ViewModel/BindingHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace API.ViewModel
+{
+    public class BindingHandler : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
